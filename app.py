1ff945fa33c568c27e0a4a2e4186dc3e69478e69
Adarsh Raj <77489636+Adarsh-Raj04@users.noreply.github.com>
2023-05-26 11:55:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import numpy as np
 import string
 import pickle 
 st.set_option('deprecation.showfileuploaderEncoding',False)
-model = pickle.load(open("new_model.pk1','rd'))
+model = pickle.load(open('new_model.pk1','rd'))
 def main():
     html_temp = """
     <div style ="background-color:lightblue;padding:16px">
