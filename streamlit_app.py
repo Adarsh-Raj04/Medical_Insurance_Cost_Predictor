75f9e54477f596b0d18aff52a17c88c054b902c5
Adarsh Raj <77489636+Adarsh-Raj04@users.noreply.github.com>
2023-11-27 04:50:26 +0530

    made it more interactive

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,48 +7,66 @@ Created on Sun Apr 16 16:00:58 2023
 
 import streamlit as st
 import numpy as np
-import string
-import pickle 
-st.set_option('deprecation.showfileUploaderEncoding',False)
+import pickle
+
+# Load the model outside the main function
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
-def main():
-    html_temp = """
-    <div style ="background-color:lightblue;padding:16px">
-    <h2 style ='color:black';text-align:center>Health insurance cost predictor</h2>
-    </div>
-    
+
+# CSS styling
+st.markdown(
     """
-    st.markdown(html_temp,unsafe_allow_html=True)
+    <style>
+        .main {
+            background-color: #f0f8ff; /* Light Blue */
+            padding: 16px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h2 {
+            color: #000;
+            text-align: center;
+        }
+        .footer {
+            text-align: center;
+            margin-top: 20px;
+            font-size: 12px;
+            color: #777;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+def main():
+    st.markdown("<h2>Health Insurance Cost Predictor</h2>", unsafe_allow_html=True)
+
+    p1 = st.slider("Enter your age", 18, 100)
     
-    with open('model.pkl', 'rb') as file:
-        model = pickle.load(file)
-    p1=st.slider("Enter your age",18,100)
+    s1 = st.radio('Select gender', ('Male', 'Female'))
+    p2 = 1 if s1 == 'Male' else 0
     
-    s1 = st.selectbox('sex',('male','female'))
-    if s1 =='male':
-        p2=1
-    else:
-        p2=0
-        
-    p3=st.number_input("Enter your BMI value")
+    p3 = st.number_input("Enter your BMI value", format="%f", step=0.1)
+
+    p4 = st.slider("Enter number of children", 0, 4)
     
-    p4 = st.slider("Enter number of children",0,4)
-    p5=st.selectbox("Smoker",("Yes","No"))
-    
-    if s1 =='Yes':
-        
-        p5=1
-    else:
-        p5=0
-        
-    p6=st.slider("Enter your region[select 'southwest':1,'southeast':2,'northwest':3,'northeast':4]",1,4)
-    
+    p5 = st.selectbox("Are you a smoker?", ("No", "Yes"))
+    p5 = 1 if p5 == 'Yes' else 0
+
+    p6 = st.slider(
+        "Select your region",
+        options=["Southwest", "Southeast", "Northwest", "Northeast"],
+        value=1,
+    )
+
     if st.button('Predict'):
-        pred= model.predict([[p1,p2,p3,p4,p5,p6]])
-        st.success('Your insurance cost is {}'.format(round(pred[0],2)))
-    
-    
-   
-if __name__=='__main__':
+        pred = model.predict([[p1, p2, p3, p4, p5, p6]])
+        st.success('Your estimated insurance cost is ${:.2f}'.format(pred[0]))
+
+    st.markdown(
+        "<div class='footer'>Made with ❤️ by Adarsh</div>",
+        unsafe_allow_html=True
+    )
+
+if __name__ == '__main__':
     main()
