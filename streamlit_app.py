2d0aaf121e11a658a9e996c554346e4fe291d774
Adarsh Raj <77489636+Adarsh-Raj04@users.noreply.github.com>
2023-11-27 04:51:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.markdown(
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
         h2 {
-            color: #000;
+            color: black;
             text-align: center;
         }
         .footer {
