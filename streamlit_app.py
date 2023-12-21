fed441e9f0fcfb64db7a9ce26cda3c9de820d04f
Adarsh Raj <77489636+Adarsh-Raj04@users.noreply.github.com>
2023-12-21 10:41:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,13 @@ Created on Sun Apr 16 16:00:58 2023
 
 import streamlit as st
 import numpy as np
-import string
 import pickle 
-st.set_option('deprecation.showfileUploaderEncoding',False)
+
+# Remove the deprecated option
+# st.set_option('deprecation.showfileUploaderEncoding', False)
+
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
-
 
 # CSS styling
 st.markdown(
@@ -39,47 +40,44 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
 def main():
     html_temp = """
     <div style ="background-color:lightblue;padding:16px">
     <h2 style ='color:black';text-align:center>Adarsh Medical Insurance Cost Predictor App</h2>
     </div>
-    
     """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    
+    st.markdown(html_temp, unsafe_allow_html=True)
+
     with open('model.pkl', 'rb') as file:
         model = pickle.load(file)
-    p1=st.slider("Enter your age",18,100)
-    
-    s1 = st.selectbox('sex',('male','female'))
-    if s1 =='male':
-        p2=1
+
+    p1 = st.slider("Enter your age", 18, 100)
+    s1 = st.selectbox('sex', ('male', 'female'))
+
+    if s1 == 'male':
+        p2 = 1
     else:
-        p2=0
-        
-    p3=st.number_input("Enter your BMI value")
-    
-    p4 = st.slider("Enter number of children",0,4)
-    p5=st.selectbox("Smoker",("Yes","No"))
-    
-    if s1 =='Yes':
-        
-        p5=1
+        p2 = 0
+
+    p3 = st.number_input("Enter your BMI value")
+    p4 = st.slider("Enter number of children", 0, 4)
+    p5 = st.selectbox("Smoker", ("Yes", "No"))
+
+    if p5 == 'Yes':
+        p5 = 1
     else:
-        p5=0
-        
-    p6=st.slider("Enter your region[select 'southwest':1,'southeast':2,'northwest':3,'northeast':4]",1,4)
-    
+        p5 = 0
+
+    p6 = st.slider("Enter your region[select 'southwest':1,'southeast':2,'northwest':3,'northeast':4]", 1, 4)
+
     if st.button('Predict'):
-        pred= model.predict([[p1,p2,p3,p4,p5,p6]])
-        st.success('Your insurance cost is {}'.format(round(pred[0],2)))
-    
+        pred = model.predict([[p1, p2, p3, p4, p5, p6]])
+        st.success('Your insurance cost is {}'.format(round(pred[0], 2)))
+
     st.markdown(
         "<div class='footer'>Made with ❤️ by Adarsh</div>",
         unsafe_allow_html=True
     )
-   
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
